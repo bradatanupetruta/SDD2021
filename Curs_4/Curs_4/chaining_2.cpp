cd26a4090e4e96de11a575a412936c97d110246a
CRISTIAN CIUREA <cristianciurea@gmail.com>
2021-03-15 15:46:59 +0200

    update

---
+++ Curs_4/Curs_4/chaining_2.cpp
@@ -0,0 +1,266 @@
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+using namespace std;
+
+struct student
+{
+	int cod;
+	char* nume;
+	float medie;
+};
+
+struct nodLS
+{
+	student inf;
+	nodLS* next;
+};
+
+struct hashT
+{
+	nodLS** vect;
+	int size;
+};
+
+int functieHash(char cheie[20], hashT tabela)
+{
+	return cheie[0] % tabela.size;
+}
+
+int inserare(hashT tabela, student s)
+{
+	int pozitie = 0;
+	if (tabela.vect != NULL)
+	{
+		pozitie = functieHash(s.nume, tabela);
+		nodLS* nou = (nodLS*)malloc(sizeof(nodLS));
+		nou->inf.cod = s.cod;
+		nou->inf.nume = (char*)malloc((strlen(s.nume) + 1) * sizeof(char));
+		strcpy(nou->inf.nume, s.nume);
+		nou->inf.medie = s.medie;
+		nou->next = NULL;
+		if (tabela.vect[pozitie] == NULL)
+			tabela.vect[pozitie] = nou;
+		else
+		{
+			nodLS* temp = tabela.vect[pozitie];
+			while (temp->next)
+				temp = temp->next;
+			temp->next = nou;
+		}
+	}
+	return pozitie;
+}
+
+void traversareLista(nodLS* cap)
+{
+	nodLS* temp = cap;
+	while (temp)
+	{
+		cout << "Cod: " << temp->inf.cod
+			<< "\nnume: " << temp->inf.nume
+			<< "\nmedie: " << temp->inf.medie
+			<< endl;
+
+		temp = temp->next;
+	}
+}
+
+void traversareTabela(hashT tabela)
+{
+	if (tabela.vect != nullptr)
+	{
+		for (int i = 0; i < tabela.size; i++)
+		{
+			if (tabela.vect[i] != nullptr)
+			{
+				cout << "Pozitie: " << i << endl;
+				traversareLista(tabela.vect[i]);
+			}
+		}
+	}
+}
+
+void traversare(hashT tabela)
+{
+	if (tabela.vect != NULL)
+	{
+		for (int i = 0; i < tabela.size; i++)
+			if (tabela.vect[i] != NULL)
+			{
+				printf("\nPozitie=%d", i);
+				nodLS* temp = tabela.vect[i];
+				while (temp)
+				{
+					printf("\nCod=%d, Nume=%s, Medie=%5.2f",
+						temp->inf.cod, temp->inf.nume, temp->inf.medie);
+					temp = temp->next;
+				}
+			}
+	}
+}
+
+void dezalocareLista(nodLS* cap)
+{
+	nodLS* temp = cap;
+	while (temp)
+	{
+		nodLS* temp2 = temp->next;
+		delete[] temp->inf.nume;
+		delete temp;
+		temp = temp2;
+	}
+}
+
+void dezalocareTabela(hashT tabela)
+{
+	if (tabela.vect != nullptr)
+	{
+		for (int i = 0; i < tabela.size; i++)
+		{
+			if (tabela.vect[i] != nullptr)
+			{
+				dezalocareLista(tabela.vect[i]);
+			}
+		}
+		delete[] tabela.vect;
+	}
+}
+
+void dezalocare(hashT tabela)
+{
+	if (tabela.vect != NULL)
+	{
+		for (int i = 0; i < tabela.size; i++)
+			if (tabela.vect[i] != NULL)
+			{
+				nodLS* temp = tabela.vect[i];
+				while (temp)
+				{
+					nodLS* temp2 = temp->next;
+					free(temp->inf.nume);
+					free(temp);
+					temp = temp2;
+				}
+			}
+		free(tabela.vect);
+	}
+}
+
+void stergeNod(nodLS* nod)
+{
+	delete[] nod->inf.nume;
+	delete nod;
+}
+
+int stergere(hashT tabela, char nume[20])
+{
+	int pozitie = 0;
+	if (tabela.vect != NULL)
+	{
+		pozitie = functieHash(nume, tabela);
+		if (tabela.vect[pozitie] == NULL)
+			return -1;
+		else
+		{
+			if (strcmp(tabela.vect[pozitie]->inf.nume, nume) == 0)
+			{
+				if (tabela.vect[pozitie]->next == NULL)
+				{
+					/*nodLS* temp = tabela.vect[pozitie];
+					free(temp->inf.nume);
+					free(temp);*/
+					stergeNod(tabela.vect[pozitie]);
+					tabela.vect[pozitie] = NULL;
+				}
+				else
+				{
+					nodLS* temp = tabela.vect[pozitie];
+					tabela.vect[pozitie] = temp->next;
+					/*free(temp->inf.nume);
+					free(temp);*/
+					stergeNod(temp);
+				}
+			}
+			else
+			{
+				nodLS* temp = tabela.vect[pozitie];
+				while (temp->next != NULL && strcmp(temp->next->inf.nume, nume) != 0)
+					temp = temp->next;
+				nodLS* p = temp->next;
+				if (p->next != NULL)
+				{
+					temp->next = p->next;
+					/*free(p->inf.nume);
+					free(p);*/
+					stergeNod(p);
+				}
+				else
+				{
+					temp->next = NULL;
+					/*free(p->inf.nume);
+					free(p);*/
+					stergeNod(p);
+				}
+			}
+		}
+	}
+	return pozitie;
+}
+
+void main()
+{
+	hashT tabela;
+	tabela.size = 101;
+	tabela.vect = (nodLS**)malloc(tabela.size * sizeof(nodLS*));
+	for (int i = 0; i < tabela.size; i++)
+		tabela.vect[i] = NULL;
+
+	int n;
+	/*printf("Nr. studenti=");
+	scanf("%d", &n);
+	student s;
+	char buffer[20];
+	for (int i = 0; i < n; i++)
+	{
+		printf("\nCod=");
+		scanf("%d", &s.cod);
+		printf("\nNume=");
+		scanf("%s", buffer);
+		s.nume = (char*)malloc((strlen(buffer) + 1) * sizeof(char));
+		strcpy(s.nume, buffer);
+		printf("\nMedie=");
+		scanf("%f", &s.medie);
+
+		inserare(tabela, s);
+	}*/
+
+	ifstream fisier;
+	fisier.open("fisier.txt");
+	student s;
+	char buffer[20];
+	fisier >> n;
+	for (int i = 0; i < n; i++)
+	{
+		fisier >> s.cod;
+		fisier >> buffer;
+		s.nume = (char*)malloc((strlen(buffer) + 1) * sizeof(char));
+		strcpy(s.nume, buffer);
+		fisier >> s.medie;
+		inserare(tabela, s);
+	}
+	fisier.close();
+
+	traversare(tabela);
+
+	char buffer2[20];
+	cout << "\nNume de sters: "; 
+	cin >> buffer2;
+	stergere(tabela, buffer2);
+
+	printf("\n------dupa stergere-------");
+
+	traversare(tabela);
+
+	dezalocare(tabela);
+}
---
+++ Curs_4/Curs_4/lista_liste.cpp
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct student
+{
+	int varsta;
+	char* nume;
+	float medie;
+};
+
+struct nodLS
+{
+	student inf;
+	nodLS* next;
+};
+
+struct nodLP
+{
+	nodLS* inf;
+	nodLP* next;
+};
+
+void inserareLS(nodLS** cap, student s)
+{
+	nodLS* nou = (nodLS*)malloc(sizeof(nodLS));
+	nou->inf.varsta = s.varsta;
+	nou->inf.nume = (char*)malloc((strlen(s.nume) + 1) * sizeof(char));
+	strcpy(nou->inf.nume, s.nume);
+	nou->inf.medie = s.medie;
+	nou->next = NULL;
+	if (*cap == NULL)
+		*cap = nou;
+	else
+	{
+		nodLS* temp = *cap;
+		while (temp->next)
+			temp = temp->next;
+		temp->next = nou;
+	}
+}
+
+void inserareLP(nodLP** capLP, nodLS* capLS)
+{
+	nodLP* nou = (nodLP*)malloc(sizeof(nodLP));
+	nou->inf = capLS;
+	nou->next = NULL;
+	if (*capLP == NULL)
+		*capLP = nou;
+	else
+	{
+		nodLP* temp = *capLP;
+		while (temp->next)
+			temp = temp->next;
+		temp->next = nou;
+	}
+}
+
+void traversareLS(nodLS* cap)
+{
+	nodLS* temp = cap;
+	while (temp)
+	{
+		printf("\nVarsta=%d, Nume=%s, Medie=%5.2f", temp->inf.varsta, temp->inf.nume, temp->inf.medie);
+		temp = temp->next;
+	}
+}
+
+void traversareLP(nodLP* cap)
+{
+	nodLP* temp = cap;
+	while (temp)
+	{
+		printf("\nSublista: ");
+		traversareLS(temp->inf);
+		temp = temp->next;
+	}
+}
+
+void dezalocareLS(nodLS* cap)
+{
+	nodLS* temp = cap;
+	while (temp)
+	{
+		nodLS* temp2 = temp->next;
+		free(temp->inf.nume);
+		free(temp);
+		temp = temp2;
+	}
+}
+
+void dezalocareLP(nodLP* cap)
+{
+	nodLP* temp = cap;
+	while (temp)
+	{
+		nodLP* temp2 = temp->next;
+		dezalocareLS(temp->inf);
+		temp = temp2;
+	}
+}
+
+void main()
+{
+	int n;
+	/*printf("Nr. studenti=");
+	scanf("%d", &n);*/
+	student s;
+	char buffer[20];
+	nodLP* capLP = NULL;
+	nodLS* capLSpar = NULL, * capLSimpar = NULL;
+
+	ifstream fisier;
+	fisier.open("fisier.txt");
+	
+	fisier >> n;
+	for (int i = 0; i < n; i++)
+	{
+		/*printf("\nVarsta=");
+		scanf("%d", &s.varsta);
+		printf("\nNume=");
+		scanf("%s", buffer);
+		s.nume = (char*)malloc((strlen(buffer) + 1) * sizeof(char));
+		strcpy(s.nume, buffer);
+		printf("\nMedie=");
+		scanf("%f", &s.medie);*/
+		fisier >> s.varsta;
+		fisier >> buffer;
+		s.nume = (char*)malloc((strlen(buffer) + 1) * sizeof(char));
+		strcpy(s.nume, buffer);
+		fisier >> s.medie;
+
+		if (s.varsta % 2 == 0)
+			inserareLS(&capLSpar, s);
+		else
+			inserareLS(&capLSimpar, s);
+
+	}
+	fisier.close();
+
+	inserareLP(&capLP, capLSpar);
+	inserareLP(&capLP, capLSimpar);
+
+	traversareLP(capLP);
+	dezalocareLP(capLP);
+
+}
